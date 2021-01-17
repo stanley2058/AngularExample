92df45c6edcda92de0de81a891a76c897e10b77c
stanley2058 <stanley2058@yahoo.com.tw>
2021-01-17 20:53:04 +0800

    update 4

--- src/app/todo-page/todo-page.component.spec.ts
+++ src/app/todo-page/todo-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TodoPageComponent } from './todo-page.component';
 
@@ -6,7 +6,7 @@ describe('TodoPageComponent', () => {
   let component: TodoPageComponent;
   let fixture: ComponentFixture<TodoPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TodoPageComponent ]
     })
